032d3e76c73ad1db78c80988439f4846fe5e79b6
The-Muhammad-Musa <126743241+The-Muhammad-Musa@users.noreply.github.com>
2023-06-29 15:49:02 -0400

    First Push

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Warrior_Proper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fighter testFighter = new fighter("test", 100, 25);
+            WriteLine(testFighter.Health);
+            testFighter.Health = 50;
+            WriteLine(testFighter.Health);
+        }
+    }
+}
---
+++ fighter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warrior_Proper
+{
+    public class fighter
+    {
+        // Variables!
+        string name;
+        int health,
+            power;
+        // Main class
+        public fighter(string pName, int pHealth, int pPower)
+        {
+            // Defining basics
+            name = pName;
+            health = pHealth;
+            power = pPower;
+        }
+
+        // Properties
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health = value;
+            }
+        }
+        public int Power
+        {
+            get
+            {
+                return power;
+            }
+            set
+            {
+                power = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
